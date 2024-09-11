f6578670e78b3bad58504dec61ccae2963dadbf0
Nayab829 <nayabg829@gmail.com>
2024-09-11 15:28:11 +0500

    add stop watch

---
+++ Stop_Watch/app.js
@@ -0,0 +1,53 @@
+const stopWatch = document.querySelector(".stopwatch");
+const startBtn = document.getElementById("startBtn");
+const stopBtn = document.getElementById("stopBtn");
+const resetBtn = document.getElementById("resetBtn");
+
+let msec = 00;
+let sec = 00;
+let min = 00;
+let hrs = 00;
+
+let timerId = null;
+
+startBtn.addEventListener("click", () => {
+    if (timerId !== null) {
+        clearInterval(timerId);
+    }
+    timerId = setInterval(startTimer, 10);
+
+
+
+})
+stopBtn.addEventListener("click", () => {
+    clearInterval(timerId)
+
+
+})
+resetBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+    stopWatch.innerHTML = `00 : 00 : 00 : 00`;
+    msec = sec = min = hrs =00;
+
+})
+
+const startTimer = () => {
+    msec++;
+    if (msec == 100) {
+        msec = 0;
+        sec++;
+        if (sec == 60) {
+            sec = 0;
+            min++;
+            if (min == 60) {
+                min = 0;
+                hrs++
+            }
+        }
+    }
+    let msecString = msec < 10 ? `0${msec} ` : msec;
+    let secString = sec < 10 ? `0${sec}` : sec;
+    let minString = min < 10 ? `0${min} ` : min;
+    let hrsString = hrs < 10 ? `0${hrs} ` : hrs;
+    stopWatch.innerHTML = `${hrsString} :${minString} :${secString}: ${msecString}`;
+}
